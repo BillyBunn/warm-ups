3cbb029269fe5d2c7c821813eb5eb6e8289c3121
Billy Bunn <williammbunn@gmail.com>
2019-03-25 16:01:17 -0700

    adds array methods

--- warmup-daily/class-1.js
+++ warmup-daily/class-1.js
@@ -1,1 +1,63 @@
 'use strict';
+
+// Arrays
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const forLoop = (arr) => {
+  for (let i = 0; i < arr.length; i++) console.log(arr[i]);
+}
+forLoop(numbers); //?
+
+const whileLoop = (arr) => {
+  let i = 0;
+  while (i < arr.length) {
+    console.log(arr[i])
+    i++;
+  }
+}
+// test
+whileLoop(numbers);
+
+const map = (arr, cb) => {
+  const output = [];
+  for (let i = 0; i < arr.length; i++) {
+    output.push(cb(arr[i]));
+  }
+  return output;
+}
+// test
+map(numbers, (val) => val - 1); //?
+
+const filter = (arr, cb) => {
+  const output = [];
+  for (let i = 0; i < arr.length; i++) {
+    cb(arr[i]); //?
+    if (cb(arr[i])) {
+      output.push(arr[i]); //?
+    }
+  }
+  return output;
+}
+// test
+filter(numbers, (val) => {if (val < 6) return true}); //?
+
+const reduce = (arr, cb, initValue) => {
+  initValue;
+  let accumulator = initValue;
+  accumulator;
+
+  for (let i = 0; i < arr.length; i++) {
+    arr[i]; //?
+    accumulator = cb(accumulator, arr[i], i);
+  }
+  return accumulator;
+}
+// test
+reduce(numbers, (acc, val, idx) => {
+  acc.values.push(val);
+  acc.indexes.push(idx);
+  return acc;
+}, {
+  values: [],
+  indexes: []
+});
