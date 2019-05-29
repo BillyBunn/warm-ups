5de381916687854c1b096a6a073858a40ef4d421
Billy Bunn <williammbunn@gmail.com>
2019-05-29 08:26:09 -0700

    updates template

--- warmup-daily/.template.js
+++ warmup-daily/.template.js
@@ -19,6 +19,8 @@
 // Objects
 
 // Begin with the starter code below ...
+const people = ['Kookla','Fran','Ollie'];
+
 const stuff = {
   tv: 'huge',
   radio: 'old',
@@ -26,28 +28,22 @@ const stuff = {
   cars: ['Toyota','Mazda']
 }
 
-let state = {};
+// Using spread and destructuring assignment, create a new array called `newPeople', which is a copy of the `people` array, with a person named 'Odie' added to the beginning and one named 'Garfield' added to the end.
 
 let newPeople = [];
+
+// Using spread and destructuring assignment, create a new object called `newStuff', which is a copy of the `stuff` object, with a new car added to the end of the `cars` array within it
+
 const newStuff = {};
-let newState = {};
 
-// Using spread and destructuring assignment, create a new array called newPeople', which is a copy of the people` array, with a person named 'Odie' added to the beginning and one named 'Garfield' added to the end.
-
-
-
-// Using spread and destructuring assignment, create a new object called newStuff', which is a copy of the stuffobject, with a new car added to the end of thecars` array within it
-
-
-
-// Create a state object with keys of people and stuff that contain the people and stuff data.
+// Create a `state` object with keys of people and stuff that contain the `people` and `stuff` data.
 // Do this using object destructuring assignment
 
+let state = {};
 
+// Using spread and destructuring assignments, create a new object called `newSate`, repeating the `newPeople` and `newStuff` steps above but directly within the people and stuff nodes of the state object (don't just spread in `newPeople` and `newStuff`)
 
-// Using spread and destructuring assignments, create a new object called newSate, repeating the newPeople and newStuff steps above but directly within the people and stuff nodes of the state object (don't just spread in newPeople and newStuff)
-
-
+let newState = {};
 
 // Prove that the original people, stuff, and state are unchanged.
 // const people = ['Kookla','Fran','Ollie'];
