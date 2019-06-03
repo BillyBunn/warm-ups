2cab6cd47aace054b3652ee3841e378b43a6f1d6
Billy Bunn <williammbunn@gmail.com>
2019-06-03 09:05:37 -0700

    completes basic requirements in a single file

--- warmup-02/06-03-19/index.js
+++ warmup-02/06-03-19/index.js
@@ -1,14 +1,44 @@
-// Requiring the express dependency
+// SERVER CONFIGURATION ----------------------------------------
+// Brings in environment variables
+require('dotenv').config();
+
+// Brings in the express dependency
 const express = require('express');
 
-// Invoke express and call it "app"
+// Invokes express and call it "app"
 const app = express();
 
-// A hard-coded port number where the server will be up and listening
-const PORT = 3000;
+// Gets the port number from the env variables
+const PORT = process.env.PORT || 3000;
 
-// A route listener and handler for the '/' route
+// MIDDLEWARE ----------------------------------------
+// Serves static files (images, CSS files, and JavaScript files) from the 'public' directory
+app.use(express.static('public'));
+
+// Parses incoming requests with JSON payloads
+app.use(express.json());
+
+// ROUTES ----------------------------------------
 app.get('/', (req, res) => res.send('Hello'));
+
+// POST route with a JSON response containing the data posted to the server
+app.post('/save', (req, res) => {
+  res.json(req.body)
+})
+
+// Route test the error handler
+app.get('/foo', (req, res, next) => next('foo'));
+
+// Catch-all route, 404
+app.get('*', (req, res) => {
+  res.status(404).send('404 page not found');
+});
+
+// Error route, 500
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('500 server error');
+});
 
 // Gets the server up and running
 app.listen(PORT, () => console.log(`Server up on PORT ${PORT}`));
